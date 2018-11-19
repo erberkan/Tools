cdcba8fccbeb50926712905409f076262bc90643
Berkan <erberkan@hotmail.com>
2018-11-19 19:01:50 +0300

    Krack v1

---
+++ krack.py
@@ -0,0 +1,29 @@
+import sys
+import zipfile 
+ 
+def crack(x,y):
+    password = None 
+    file_to_open = zipfile.ZipFile(x) 
+    with open(y, 'r') as f: 
+        for line in f.readlines(): 
+            password = line.strip('\n') 
+            try: 
+               file_to_open.extractall(pwd=password) 
+               password = 'Password found: %s' % password 
+               print password 
+            except: 
+                pass 
+
+
+def main():
+    try:
+        filename = sys.argv[1]
+        wordlist = sys.argv[2]
+        crack(filename, wordlist)
+    except:
+        print("\nUsage: python krack.py <file> <wordlist>\n")
+        print("Example: python krack.py secret.zip wordlist.txt\n")
+
+
+if __name__ == "__main__":
+    main()
